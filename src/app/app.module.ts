9a8bbe4b105d8a6c43478de14cc8c1223a4d32a6
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-10 22:52:05 +0530

    View All

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddCourseComponent } from './add-course/add-course.component';
+import { ViewAllComponent } from './view-all/view-all.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddCourseComponent
+    AddCourseComponent,
+    ViewAllComponent
   ],
   imports: [
     BrowserModule,
